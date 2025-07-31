5dd684e4f9ae0e442adc6d7a4c4b9c00751eff63
Alexander Komyagin <adkomyagin@gmail.com>
2025-07-31 10:20:46 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,36 @@
+module hbase-kafka-stats-tool
+
+go 1.24
+
+toolchain go1.24.3
+
+require github.com/IBM/sarama v1.45.2
+
+require (
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.7.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/hamba/avro/v2 v2.29.0
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/hamba/avro/v2"
+)
+
+var AvroSchema = avro.MustParse(`{"namespace": "org.apache.hadoop.hbase.kafka",
+ "type": "record",
+ "name": "HBaseKafkaEvent",
+ "fields": [
+    {"name": "key", "type": "bytes"},
+    {"name": "timestamp",  "type": "long" },
+    {"name": "delete",  "type": "boolean" },
+    {"name": "value", "type": "bytes"},
+    {"name": "qualifier", "type": "bytes"},
+    {"name": "family", "type": "bytes"},
+    {"name": "table", "type": "bytes"}
+ ]
+}`)
+
+type HbaseKafkaEvent struct {
+	Key       []byte `avro:"key"`
+	Timestamp int64  `avro:"timestamp"`
+	Delete    bool   `avro:"delete"`
+	Value     []byte `avro:"value"`
+	Qualifier []byte `avro:"qualifier"`
+	Family    []byte `avro:"family"`
+	Table     []byte `avro:"table"`
+}
+
+type Stats struct {
+	Count      int
+	TotalBytes int
+}
+
+func newKafkaConfig(user, password string) *sarama.Config {
+	cfg := sarama.NewConfig()
+	cfg.Consumer.Return.Errors = true
+	cfg.Version = sarama.V2_1_0_0
+	if user != "" {
+		cfg.Net.TLS.Enable = true
+		cfg.Net.SASL.Enable = true
+		cfg.Net.SASL.Mechanism = sarama.SASLMechanism(sarama.SASLTypePlaintext)
+		cfg.Net.SASL.User = user
+		cfg.Net.SASL.Password = password
+	}
+	return cfg
+}
+
+func main() {
+	broker := flag.String("broker", "", "Kafka broker address")
+	topic := flag.String("topic", "", "Kafka topic")
+	user := flag.String("user", "", "Kafka username (optional)")
+	password := flag.String("password", "", "Kafka password (optional)")
+	duration := flag.Int("duration", 10, "Seconds to consume messages")
+	flag.Parse()
+
+	if *broker == "" || *topic == "" {
+		fmt.Printf("Usage: %s -broker <broker> -topic <topic> [-user <user>] [-password <password>] [-duration <seconds>]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	config := newKafkaConfig(*user, *password)
+	client, err := sarama.NewClient([]string{*broker}, config)
+	if err != nil {
+		log.Fatalf("Failed to create Kafka client: %v", err)
+	}
+	defer client.Close()
+
+	partitions, err := client.Partitions(*topic)
+	if err != nil {
+		log.Fatalf("Failed to get partitions: %v", err)
+	}
+
+	stats := make(map[string]*Stats)
+	consumer, err := sarama.NewConsumerFromClient(client)
+	if err != nil {
+		log.Fatalf("Failed to create consumer: %v", err)
+	}
+	defer consumer.Close()
+
+	done := make(chan struct{})
+	for _, partition := range partitions {
+		pc, err := consumer.ConsumePartition(*topic, partition, sarama.OffsetOldest)
+		if err != nil {
+			log.Fatalf("Failed to consume partition %d: %v", partition, err)
+		}
+		go func(pc sarama.PartitionConsumer) {
+			for message := range pc.Messages() {
+				var event HbaseKafkaEvent
+				if err := avro.Unmarshal(AvroSchema, message.Value, &event); err != nil {
+					log.Printf("Failed to decode message: %v", err)
+					continue
+				}
+				ns := string(event.Table)
+				size := len(message.Value)
+				if stats[ns] == nil {
+					stats[ns] = &Stats{}
+				}
+				stats[ns].Count++
+				stats[ns].TotalBytes += size
+			}
+			done <- struct{}{}
+		}(pc)
+	}
+
+	fmt.Printf("Consuming messages for %d seconds...\n", *duration)
+	time.Sleep(time.Duration(*duration) * time.Second)
+
+	fmt.Println("Namespace statistics:")
+	for ns, s := range stats {
+		avg := 0
+		if s.Count > 0 {
+			avg = s.TotalBytes / s.Count
+		}
+		fmt.Printf("Namespace: %s, Events: %d, Avg Size: %d bytes, Total Size: %d bytes\n", ns, s.Count, avg, s.TotalBytes)
+	}
+}
